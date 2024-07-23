002951976542c2172a39dc38a1385b93f4dbacbb
Geoffroy Evane <soumailaevane@gmail.com>
2024-07-23 13:09:40 +0000

    chore: ...

--- app/core/settings.py
+++ app/core/settings.py
@@ -49,8 +49,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 # Application definition
 
 INSTALLED_APPS = [
-    "admin_interface",
-    "colorfield",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
